be5d2b2f60abbad4748fdf2ae3905c800ccd46c5
Jan Glinko <jan.glinko@pomagier.info>
2021-12-07 15:44:42 +0100

    Add detectron

---
+++ detectron2/train_new.py
@@ -0,0 +1,67 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.data.datasets import register_coco_instances
+register_coco_instances("my_dataset_val", {}, "/home/avena/PycharmProjects/pythonProject/containers_001/coco_annotations.json", "/home/avena/PycharmProjects/pythonProject/containers_001")
+register_coco_instances("my_dataset_train", {}, "/home/avena/PycharmProjects/pythonProject/standard_002/coco_annotations.json", "/home/avena/PycharmProjects/pythonProject/standard_002")
+
+from detectron2.engine import DefaultTrainer
+
+
+cfg = get_cfg()
+cfg.INPUT.MASK_FORMAT = "bitmask"
+
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("my_dataset_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = "/home/avena/PycharmProjects/pythonProject/detectron/output/model_final.pth"  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 1500    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 26  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)
+trainer.train()
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.05   # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+
+
+from detectron2.utils.visualizer import ColorMode
+dataset_dicts_metadata = MetadataCatalog.get("my_dataset_train")
+dataset_dicts = DatasetCatalog.get("my_dataset_train")
+for d in random.sample(dataset_dicts, 5):
+    im = cv2.imread(d["file_name"])
+    outputs = predictor(im)  # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
+    v = Visualizer(im[:, :, ::-1],
+                   metadata=dataset_dicts_metadata,
+                   scale=0.5,
+                   instance_mode=ColorMode.IMAGE_BW   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+    )
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imshow("", out.get_image()[:, :, ::-1])
+    cv2.waitKey(0)
+
+# from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+# from detectron2.data import build_detection_test_loader
+# evaluator = COCOEvaluator("my_dataset_train", output_dir="./output")
+# val_loader = build_detection_test_loader(cfg, "my_dataset_train")
+# print(inference_on_dataset(predictor.model, val_loader, evaluator))
